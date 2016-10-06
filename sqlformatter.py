2ea0403c421ac2b317ea31a9cfa8b16cc7412873
Henrique Bastos <henrique@bastos.net>
2016-10-06 20:28:09 -0300

    Closes #1 Windows does not support highlight.

--- sqlformatter.py
+++ sqlformatter.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+import os
 import sys
 import logging
 import sqlparse
@@ -63,4 +64,8 @@ class LogDb:
 
         return self.handler
 
-logdb = LogDb()
+
+if os.name == 'nt':
+    logdb = LogDb(highlight=False)
+else:
+    logdb = LogDb()
