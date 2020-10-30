f3b025afbe0ce5deecd09321fc63ba81b9213bbd
Henrique Bastos <henrique@bastos.net>
2020-10-30 11:54:22 -0300

    Bump version 1.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = os.path.join(os.path.dirname(__file__), 'README.rst')
 REQUIREMENTS = os.path.join(os.path.dirname(__file__), 'requirements.txt')
 
 setup(name='sqlformatter',
-      version='1.3',
+      version='1.4',
       description='SQLFormatter: Beautiful colored SQL staments for logging',
       long_description=open(README).read(),
       author="Henrique Bastos", author_email="henrique@bastos.net",
